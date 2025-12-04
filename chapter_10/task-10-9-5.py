7bb03fdb191958c8b6547680a956138808f15780
Андрей Сапунов <jokerjam@yandex.ru>
2025-12-04 20:04:48 +0300

    2025-12-04 20:04:48 -

---
+++ chapter_10/task-10-9-5.py
@@ -0,0 +1,28 @@
+"""
+Асинхронное преобразование CSV в JSON
+"""
+import asyncio
+import json
+
+import aiocsv
+import aiofiles
+from aiofiles.base import AiofilesContextManager
+
+csv_file = './file/address_10000.csv'
+json_file = './file/address_10000.json'
+
+
+async def main():
+    json_data = []
+
+    async with aiofiles.open(csv_file, encoding='utf-8-sig') as f:
+        csv_reader = aiocsv.AsyncDictReader(f, delimiter=';')
+        async for row in csv_reader:
+            json_data.append(row)
+
+    async with aiofiles.open(json_file, encoding='utf-8', mode="w") as j:
+        await j.write(json.dumps(json_data, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
